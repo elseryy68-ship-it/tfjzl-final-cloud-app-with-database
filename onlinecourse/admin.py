4bc9f5c3b0fe07cc35b6b7b08343dca3d53ae2e3
elseryy68-ship-it <elseryy68-ship-it@users.noreply.github.com>
2026-09-08 08:42:30 -0400

    Add assessment models and admin

--- onlinecourse/admin.py
+++ onlinecourse/admin.py
@@ -1,8 +1,26 @@
 from django.contrib import admin
-# <HINT> Import any new Models here
-from .models import Course, Lesson, Instructor, Learner
 
-# <HINT> Register QuestionInline and ChoiceInline classes here
+from .models import (
+    Course,
+    Lesson,
+    Instructor,
+    Learner,
+    Enrollment,
+    Question,
+    Choice,
+    Submission,
+)
+
+
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 2
+
+
+class QuestionInline(admin.StackedInline):
+    model = Question
+    extra = 2
+    show_change_link = True
 
 
 class LessonInline(admin.StackedInline):
@@ -10,21 +28,27 @@ class LessonInline(admin.StackedInline):
     extra = 5
 
 
-# Register your models here.
 class CourseAdmin(admin.ModelAdmin):
-    inlines = [LessonInline]
+    inlines = [LessonInline, QuestionInline]
     list_display = ('name', 'pub_date')
     list_filter = ['pub_date']
     search_fields = ['name', 'description']
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [ChoiceInline]
+    list_display = ['content', 'course', 'grade']
 
 
 class LessonAdmin(admin.ModelAdmin):
     list_display = ['title']
 
 
-# <HINT> Register Question and Choice models here
-
 admin.site.register(Course, CourseAdmin)
 admin.site.register(Lesson, LessonAdmin)
 admin.site.register(Instructor)
 admin.site.register(Learner)
+admin.site.register(Enrollment)
+admin.site.register(Submission)
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
